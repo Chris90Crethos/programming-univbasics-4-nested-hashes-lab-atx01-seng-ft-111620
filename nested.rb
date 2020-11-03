472073121f608dfa1d2722bed7c7d6bb4a437989
Chris Martin <bmg.martin08@gmail.com>
2020-11-03 21:54:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,7 +60,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-    programmer_hash[:dennis_ritchie][:languages]
+    programmer_hash[:dennis_ritchie][1 ]
 programmer_hash
 end
 
